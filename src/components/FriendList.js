9c24b2b2bd301222f5272e3da2d5242908a53ac2
Pavlo Berkut <pavlo.berkut@gmail.com>
2023-02-14 22:52:18 +0200

    add for avatar default temlate image if undefined

--- src/components/FriendList.js
+++ src/components/FriendList.js
@@ -1,11 +1,17 @@
 import PropTypes from 'prop-types';
+import avatarDefault from '../images/avatarDefault.png';
 export default function FriendList({ friends }) {
   return (
     <ul className="friend-list">
       {friends.map(({ avatar, name, isOnline, id }) => (
         <li className="item" key={id}>
           <span className="status">{isOnline ? 'Online' : 'Offline'}</span>
-          <img className="avatar" src={avatar} alt="User avatar" width="48" />
+          <img
+            className="avatar"
+            src={avatar ?? avatarDefault}
+            alt="User avatar"
+            width="48"
+          />
           <p className="name">{name}</p>
         </li>
       ))}
